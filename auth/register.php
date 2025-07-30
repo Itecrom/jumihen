73a4b51fa86dbf56afd27fccbbb2b776eaaebccb
HE12ND34RESON <hendersonpromise534@gmail.com>
2025-07-30 17:39:42 +0200

    added auth details

---
+++ auth/register.php
@@ -0,0 +1,29 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Register</title>
+</head>
+<body>
+    
+<div>
+    <h1>seller sign up</h1>
+    <form action="register.php" method="post">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required><br><br>
+
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required><br><br>
+
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required><br><br>
+        
+        <label for="picture">Picture:</label>
+        <input type="file" id="picture" name="picture" accept="image/*"><br><br>
+
+        <button type="submit">Register</button>
+    </form>
+    <p>Already have an account? <a href="login.php">Login here</a>.</p>
+</div>
+</body>
+</html>
