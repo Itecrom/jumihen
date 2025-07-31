2c11482c8b98103a553f2a9729e02ee8796ddefb
Itec ICT Hub <leonardjjmhone@gmail.com>
2025-07-31 10:22:09 +0200

    Ammended Logout and Signout

--- auth/logout.php
+++ auth/logout.php
@@ -1,29 +1,6 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin logout</title>
-</head>
-<body>
-    
-<div>
-    <h1>admin logout</h1>
-    <form action="register.php" method="post">
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" required><br><br>
+<?php
+session_start();
+session_destroy();
+header('Location: ../home.php');
 
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required><br><br>
-
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required><br><br>
-        
-        <label for="picture">Picture:</label>
-        <input type="file" id="picture" name="picture" accept="image/*"><br><br>
-
-        <button type="submit">logout</button>
-    </form>
-    <p>Already have an account? <a href="login.php">Login here</a>.</p>
-</div>
-</body>
-</html>
+exit();
